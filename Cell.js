369351339e18d6d4aec2ba1a33d727665d7be151
NasonBen <nasonb@wellingtoncollege.org.uk>
2019-05-20 20:16:57 +0100

    final fix to 'completion'

--- Cell.js
+++ Cell.js
@@ -1,17 +1,25 @@
-class Cell {
-    constructor(i, j, w) {
-        this.i = i;
-        this.j = j;
+function Cell(i , j , w) {
 
-        this.w = w;
-        this.x = i * w;
-        this.y = j * w;
+    this.i = i;
+    this.j = j;
+    this.x = i * w;
+    this.y = j * w;
+    this.w = w;
+    this.neighborCount = 0;
+    this.flagged = false;
 
-        this.neighborCount = 0;
-        
-        this.flagged = false;
-        this.bee = false;
-        this.revealed = false;
+   this.bee = false
+   this.revealed = false;
+}
+
+Cell.prototype.show = function() {
+    stroke(0);
+    noFill();
+    rect(this.x,this.y,this.w,this.w);
+    if (this.flagged) {
+        fill(255, 0, 0);
+        ellipse(this.x + this.w*0.5, this.y + this.w*0.5, this.w *0.5);
+        return;
     }
     if (this.revealed){
         if (this.bee && this.flagged == false){
@@ -30,52 +38,49 @@ class Cell {
     }
 }
 
-        if (this.flagged) {
-            fill(255, 0, 0);
-            ellipse(this.x + this.w * 0.5, this.y + this.w * 0.5, this.w * 0.5);
-            return;
-        }
-        if (this.revealed) {
-            if (this.bee) {
-                fill(127);
-                ellipse(this.x + this.w * 0.5, this.y + this.w * 0.5, this.w * 0.5);
-            }
-            else {
-                fill(0);
-                rect(this.x, this.y, this.w, this.w);
-                if (this.neighborCount > 0) {
-                    textAlign(CENTER);
-                    fill(150);
-                    text(this.neighborCount, this.x + this.w * 0.5, this.y + this.w - 6);
+Cell.prototype.countBees = function() {
+    if (this.bee){
+        this.neighborCount = -1;
+        return;
+    }
+    var total = 0;
+    for (var xoff = -1; xoff <= 1; xoff ++){
+        for (var yoff = -1; yoff <= 1; yoff ++){
+            var i = this.i + xoff;
+            var j = this.j + yoff;
+            if (i > -1 && i < cols && j > -1 && j < rows){
+                var neighbor = grid[i][j];
+                if (neighbor.bee) {
+                    total++;
                 }
             }
         }
     }
+    this.neighborCount = total;
+}
 
-    countBees() {
-        if (this.bee) return;
-        this.neighborCount = 0;
-        for (let i = this.i - 1; i <= this.i + 1; i++) {
-            for (let j = this.j - 1; j <= this.j + 1; j++) {
-                if ((i > -1 && i < cols && j > -1 && j < rows) && grid[i][j].bee) this.neighborCount++;
-            }
-        }
+
+Cell.prototype.contains = function(x,y){
+    return (x > this.x && x < this.x +this.w && y > this.y && y < this.y + this.w );
+}
+Cell.prototype.reveal = function(){
+    this.revealed = true;
+    if (this.neighborCount == 0){
+        
+        // flood fill time
+        this.floodFill();
     }
+}
 
-    contains(x, y) {
-        return (x > this.x && x < this.x + this.w && y > this.y && y < this.y + this.w);
-    }
-
-    reveal() {
-        this.revealed = true;
-        if (this.neighborCount == 0) this.floodFill();
-    }
-
-    floodFill() {
-        for (let i = this.i - 1; i <= this.i + 1; i++) {
-            for (let j = this.j - 1; j <= this.j + 1; j++) {
-                if (i > -1 && i < cols && j > -1 && j < rows) {
-                    if (!grid[i][j].revealed) grid[i][j].reveal();
+Cell.prototype.floodFill = function(){
+    for (var xoff = -1; xoff <= 1; xoff ++){
+        for (var yoff = -1; yoff <= 1; yoff ++){
+            var i = this.i + xoff;
+            var j = this.j + yoff;
+            if (i > -1 && i < cols && j > -1 && j < rows){
+                var neighbor = grid[i][j];
+                if (!neighbor.bee && !neighbor.revealed){
+                    neighbor.reveal();
                 }
             }
         }
